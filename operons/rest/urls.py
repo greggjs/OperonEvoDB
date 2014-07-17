dc3ded36bdfa22d32b776ff96636a3fb2cb8d773
Andrew Oberlin <andyoberlin@gmail.com>
2014-07-16 21:20:33 -0400

    Added the bindings for the REST API to work. Just need to fill in the rest of the models and add the link into django-celery so that the job can run easily.

--- operons/rest/urls.py
+++ operons/rest/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from views import RequestResultAPI, RequestJobListAPI, RequestJobAPI
+
 urlpatterns = patterns('',
+    url(r'^results/(\d+)/?$', RequestResultAPI.as_view(), name="Request Result API View"),
+    url(r'^jobs/(\d+)/?$', RequestJobAPI.as_view(), name="Request Job API View"),
+    url(r'^jobs/?$', RequestJobListAPI.as_view(), name="Request Job List API View")
 )
--- operons/rest/views.py
+++ operons/rest/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from exceptions import BadRequestException
+
+from forms import RequestJobForm, RequestJobListForm, RequestResultForm
+
 
 # Create your views here.
+class RequestResultAPI(APIView):
+    '''
+       Class for rendering the view for getting a Request's result.
+    '''
+
+    def get(self, request, request_result_id):
+        '''
+            Method for getting a RequestResult based on the id provided.
+        '''
+        params = dict((key, val) for key, val in request.QUERY_PARAMS.iteritems())
+        params['request_result_id'] = request_result_id
+        form = RequestResultForm(params)
+
+        if not form.is_valid():
+            raise BadRequestException()
+
+        return Response(form.submit(request))
+
+
+class RequestJobListAPI(APIView):
+    '''
+       Class for rendering the view for getting a Request's result.
+    '''
+
+    def post(self, request):
+        '''
+            Method for getting a RequestResult based on the id provided.
+        '''
+        params = dict((key, val) for key, val in request.DATA.iteritems())
+        params.update(request.QUERY_PARAMS)
+        form = RequestJobListForm(params)
+
+        if not form.is_valid():
+            raise BadRequestException()
+
+        return Response(form.submit(request))
+
+class RequestJobAPI(APIView):
+    '''
+       Class for rendering the view for getting a Request's job status.
+    '''
+
+    def get(self, request, request_job_id):
+        '''
+            Method for getting a RequestJob based on the id provided.
+        '''
+        params = dict((key, val) for key, val in request.QUERY_PARAMS.iteritems())
+        params['request_job_id'] = request_job_id
+        form = RequestJobForm(params)
+
+        if not form.is_valid():
+            raise BadRequestException()
+
+        return Response(form.submit(request))
